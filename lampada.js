668ce305bd66524247aa5a642f7e37b2123b2dd2
BBML-DEV <pv.marques2010@hotmail.com>
2021-09-27 17:02:35 -0300

    Exemplo para aula do professor lauro

---
+++ lampada.js
@@ -0,0 +1,7 @@
+function acesso(){
+    document.getElementById('lampada').src ="imagens/lampada_acessa.jpg";
+}
+
+function apagado(){
+    document.getElementById('lampada').src ="imagens/lampada_apagada.jpg";
+}
